8d0191c77e0116dcbb2107a883b589351a4d6e0b
rebeccahockley <hockley.r@yahoo.com>
2021-12-07 16:31:30 +0100

    Changed a variable

--- javascript/javascript1/week1/Homework.js
+++ javascript/javascript1/week1/Homework.js
@@ -1,5 +1,5 @@
 console.log("Age-ify");
 let yearOfBirth = 1997;
 let yearFuture = 2121;
-const age = yearFuture - yearOfBirth;
+let age = yearFuture - yearOfBirth;
 console.log("You will be " + age + " years old in " + yearFuture);

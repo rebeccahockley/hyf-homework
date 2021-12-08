2b6018699655682b4e7344f724a68d92f8ab3c02
rebeccahockley <hockley.r@yahoo.com>
2021-12-08 06:29:34 +0100

    Finished Task 2: Dog Calculator

---
+++ javascript/javascript1/week1/dogcalculator.js
@@ -0,0 +1,17 @@
+console.log("Dog Calculator");
+let dogYearOfBirth = 2000;
+let dogYearFuture = 2042;
+let dogYear = dogYearFuture - dogYearOfBirth;
+let dogAge = dogYear * 7;
+
+let shouldShowResultInDogYears = false;
+if (shouldShowResultInDogYears === true) {
+  console.log(
+    "Your dog will be " + dogAge + " years old in " + dogYearFuture + "."
+  );
+} else shouldShowResultInDogYears === false;
+{
+  console.log(
+    "Your dog will be " + dogYear + " human years old in " + dogYearFuture + "."
+  );
+}

f70f388bcc1c0eef8dd567c684f4f8096a20b620
rebeccahockley <hockley.r@yahoo.com>
2021-12-13 09:41:07 +0100

    Changed a let to a const

--- javascript/javascript1/week1/Homework.js
+++ javascript/javascript1/week1/Homework.js
@@ -1,5 +1,5 @@
 console.log("Age-ify");
-let yearOfBirth = 1997;
+const yearOfBirth = 1997;
 let yearFuture = 2121;
 let age = yearFuture - yearOfBirth;
-console.log("You will be " + age + " years old in " + yearFuture);
+console.log("You will be " + age + " years old in " + yearFuture + ".");

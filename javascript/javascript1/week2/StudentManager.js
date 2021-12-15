0d027311176d124f625ee9d224f2066725ca5426
rebeccahockley <hockley.r@yahoo.com>
2021-12-15 18:52:28 +0100

    Updated Version Task 4

--- javascript/javascript1/week2/StudentManager.js
+++ javascript/javascript1/week2/StudentManager.js
@@ -1,6 +1,34 @@
 const class07Students = [];
 function addStudentToClass(studentName) {
-  class07Students;
+  if (studentName == "The Queen") {
+    class07Students.push(studentName);
+  } else if (studentName == "") {
+        console.log("Name must be filled out");
+  } else if (
+    class07Students.length < 6 &&
+    class07Students.includes(studentName) === false
+  ) {
+    class07Students.push(studentName);
+  } else if (class07Students.includes(studentName) === true) {
+    console.log("Student " + studentName + " is already in the class");
+  } else {
+    console.log("Cannot add more students to class 07");
+  }
 }
 
-function getNumberOfStudents() {}
+addStudentToClass("Jon");
+addStudentToClass("Ben");
+addStudentToClass("Patricia");
+addStudentToClass("Bob");
+addStudentToClass("Susan");
+addStudentToClass("Susan");
+addStudentToClass("Matthew");
+addStudentToClass("The Queen");
+
+console.log(class07Students);
+
+function getNumberOfStudents() {
+    console.log(class07Students.length);
+}
+
+getNumberOfStudents();

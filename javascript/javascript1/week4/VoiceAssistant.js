e5bed1ff4ef50a74924bed6bd28fc933862cde38
rebeccahockley <hockley.r@yahoo.com>
2022-01-19 19:26:54 +0100

    Updated with new commands

--- javascript/javascript1/week4/VoiceAssistant.js
+++ javascript/javascript1/week4/VoiceAssistant.js
@@ -2,6 +2,7 @@ let myCommand = [];
 const namesOfUser = [];
 const toDoList = [];
 const todayDate = new Date();
+let note = [];
 
 function getReply(command) {
   myCommand = command.split(" ");
@@ -18,12 +19,24 @@ function getReply(command) {
     if (command == "Add fishing to my todo") {
       toDoList.push(myCommand[1]);
       console.log("Fishing added to your to do list");
+    } else if (command == "Add singing in the shower to my todo") {
+      for (let i = 1; i < myCommand.length - 3; i++) {
+        note.push(myCommand[i]);
+      }
+      note = note.join(" ");
+      toDoList.push(note);
+      console.log("Singing in the shower added to your to do list");
+    }
+  }
+  if (myCommand[0] == "Remove") {
+    if (command == "Remove fishing from my todo") {
+      toDoList.splice("fishing", 1);
     }
   }
   if (myCommand[0] == "Set") {
     if (command == "Set a timer for 4 minutes") {
-      console.log("Timer set for 4 minutes");
-      time = 4 * 60000;
+      console.log("Timer set for " + myCommand[4] + " minutes");
+      time = myCommand[4] * 60000;
       const timeOutCountdown = setTimeout(function countdown() {
         console.log("Timer Done!");
       }, time);
@@ -38,9 +51,19 @@ function getReply(command) {
     if (command == "What day is it today?") {
       console.log(todayDate.toDateString());
     }
+    if (command == "What is on my todo?") {
+      console.log(toDoList);
+    }
   }
 }
 
-// getReply("Hello my name is Benjamin");
-// console.log(namesOfUser);
-getReply("Set a timer for 4 minutes");
+// // getReply("Hello my name is Benjamin");
+// getReply("Add singing in the shower to my todo");
+// // console.log(note);
+// getReply("Add fishing to my todo");
+// console.log(toDoList);
+// // getReply("What is on my todo?");
+// getReply("Remove fishing from my todo");
+// console.log(toDoList);
+
+getReply("Set a timer for 2 minutes");

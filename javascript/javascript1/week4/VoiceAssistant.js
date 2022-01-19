87912edfc6c26cb9dcf852e706591b514b4578a7
rebeccahockley <hockley.r@yahoo.com>
2022-01-19 12:59:39 +0100

    Updated with 2 more commands

--- javascript/javascript1/week4/VoiceAssistant.js
+++ javascript/javascript1/week4/VoiceAssistant.js
@@ -1,6 +1,7 @@
 let myCommand = [];
 const namesOfUser = [];
 const toDoList = [];
+const todayDate = new Date();
 
 function getReply(command) {
   myCommand = command.split(" ");
@@ -13,15 +14,33 @@ function getReply(command) {
   ) {
     console.log("Nice to meet you " + namesOfUser[0]);
   }
+  if (myCommand[0] == "Add") {
+    if (command == "Add fishing to my todo") {
+      toDoList.push(myCommand[1]);
+      console.log("Fishing added to your to do list");
+    }
+  }
+  if (myCommand[0] == "Set") {
+    if (command == "Set a timer for 4 minutes") {
+      console.log("Timer set for 4 minutes");
+      time = 4 * 60000;
+      const timeOutCountdown = setTimeout(function countdown() {
+        console.log("Timer Done!");
+      }, time);
+    }
+  }
   if (myCommand[0] == "What") {
     if (command == "What is my name?" && (namesOfUser == 0) === false) {
       console.log("Your name is " + namesOfUser[0]);
-    } else {
+    } else if (command == "What is my name?" && (namesOfUser == 0) === true) {
       console.log("You have not told me your name! Please tell me your name!");
+    }
+    if (command == "What day is it today?") {
+      console.log(todayDate.toDateString());
     }
   }
 }
 
-// getReply("Hello my name is Benjamin")
+// getReply("Hello my name is Benjamin");
 // console.log(namesOfUser);
-// getReply("What is my name?");
+getReply("Set a timer for 4 minutes");

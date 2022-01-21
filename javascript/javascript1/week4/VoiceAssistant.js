a75d3fad41e2f8f55b5d55081d925dd729d2dcb0
rebeccahockley <hockley.r@yahoo.com>
2022-01-21 10:55:54 +0100

    Finished Voice Assistant Homework

--- javascript/javascript1/week4/VoiceAssistant.js
+++ javascript/javascript1/week4/VoiceAssistant.js
@@ -2,6 +2,7 @@ let myCommand = [];
 const namesOfUser = [];
 const toDoList = [];
 const todayDate = new Date();
+const operatorArray = ["+", "-", "*", "/"];
 const jokesArray = [
   "Why you never see elephants hiding up in trees? Because they are really good at it!",
   "Why are koalas not actual bears? The do not meet the koalafications!",
@@ -33,12 +34,12 @@ function getReply(command) {
   if (myCommand[0] == "Remove") {
     let removeKeyWords = myCommand.splice(1, myCommand.length - 4);
     removeKeyWords = removeKeyWords.join(" ");
-    console.log(removeKeyWords);
     for (let i = 0; i < toDoList.length; i++) {
       if (toDoList[i] === removeKeyWords) {
         toDoList.splice(i, 1);
       }
     }
+    console.log(removeKeyWords + " has been removed from your todo list");
   }
   if (myCommand[0] == "Tell") {
     const randomNumber = Math.floor(Math.random() * jokesArray.length);
@@ -46,7 +47,7 @@ function getReply(command) {
   }
   if (myCommand[0] == "Set") {
     if (myCommand[2] == "timer") {
-      console.log("Timer set for " + myCommand[4] + " minutes");
+      console.log("Timer set for " + myCommand[4] + " minute(s)");
       time = parseInt(myCommand[4]) * 60000;
       const timeOutCountdown = setTimeout(function countdown() {
         console.log("Timer Done!");
@@ -60,7 +61,7 @@ function getReply(command) {
       console.log("You have not told me your name! Please tell me your name!");
     }
     if (command == "What day is it today?") {
-      console.log(todayDate.toDateString());
+      console.log("Today is " + todayDate.toDateString());
     }
     if (command == "What is on my todo?") {
       console.log("Your to do list is:");
@@ -68,16 +69,34 @@ function getReply(command) {
         console.log(toDoList[i]);
       }
     }
+    if (operatorArray.includes(myCommand[3])) {
+      const operator = myCommand[3];
+      const number1 = parseInt(myCommand[2]);
+      const number2 = parseInt(myCommand[4]);
+      let result;
+      if (operator == "+") {
+        result = number1 + number2;
+      } else if (operator == "-") {
+        result = number1 - number2;
+      } else if (operator == "*") {
+        result = number1 * number2;
+      } else {
+        result = number1 / number2;
+      }
+      console.log("The result is " + result);
+    }
   }
 }
 
-// getReply("Hello my name is Benjamin");
+// getReply("Hello my name is Hannah");
+// getReply("What is my name?");
 // getReply("Add singing in the shower to my todo");
 // getReply("Add shop for groceries to my todo");
-// getReply("Add go fishing to my todo");
-// console.log(toDoList);
+// // getReply("Add go fishing to my todo");
+getReply("What day is it today?");
 // getReply("What is on my todo?");
-// getReply("Tell me a joke");
-// console.log(toDoList);
+// getReply("Remove singing in the shower from my todo");
+// // getReply("Tell me a joke");
+// getReply("What is 6 / 2?");
 
-// getReply("Set a timer for 1 minutes");
+// getReply("Set a timer for 1 minute");

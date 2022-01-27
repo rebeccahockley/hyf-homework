61f527fee01da2804b5a012b7dfe85a2cf87c84c
rebeccahockley <hockley.r@yahoo.com>
2022-01-27 12:57:43 +0100

    Finished Shortest Word Task

--- javascript/javascript2/week1/ShortestWord.js
+++ javascript/javascript2/week1/ShortestWord.js
@@ -1,4 +1,24 @@
-const string1 = "hello";
-console.log(string1.length);
+const arrayOfRandomWords = [
+  "coffee",
+  "bananas",
+  "week",
+  "whiteboard",
+  "eye",
+  "mouse",
+];
 
-const arrayOfRandomWords
+function findTheShortestWord(words) {
+  let lengthOfWord = words[0].length;
+  let indexOfWord = 0;
+  for (let i = 0; i < words.length; i++) {
+    // console.log("Length of shortest " + lengthOfWord);
+    // console.log("Length of StringWord " + words[i].length);
+    if (lengthOfWord > words[i].length) {
+      lengthOfWord = words[i].length;
+      indexOfWord = i;
+    }
+  }
+  return "The shortest word is: " + words[indexOfWord];
+}
+
+console.log(findTheShortestWord(arrayOfRandomWords));

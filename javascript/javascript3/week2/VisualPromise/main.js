c20864d74132b0a291760b311b866401810d49dd
rebeccahockley <hockley.r@yahoo.com>
2022-03-25 11:08:57 +0100

    Changes made from feedback

--- javascript/javascript3/week2/VisualPromise/main.js
+++ javascript/javascript3/week2/VisualPromise/main.js
@@ -2,59 +2,41 @@ const redBox = document.querySelector("ul.marks li:nth-child(1)");
 const blueBox = document.querySelector("ul.marks li:nth-child(2)");
 const greenBox = document.querySelector("ul.marks li:nth-child(3)");
 
-const redTargetX = 20 - parseInt(redBox.style.left);
-const redTargetY = 300 - parseInt(redBox.style.top);
-const blueTargetX = 400 - parseInt(blueBox.style.left);
-const blueTargetY = 300 - parseInt(blueBox.style.top);
-const greenTargetX = 400 - parseInt(greenBox.style.left);
-const greenTargetY = 20 - parseInt(greenBox.style.top);
+const redTarget = {
+  x: 20 - parseInt(redBox.style.left),
+  y: 300 - parseInt(redBox.style.top),
+};
+const blueTarget = {
+  x: 400 - parseInt(blueBox.style.left),
+  y: 300 - parseInt(blueBox.style.top),
+};
+const greenTarget = {
+  x: 400 - parseInt(greenBox.style.left),
+  y: 20 - parseInt(greenBox.style.top),
+};
 
-function translateOneByOne() {
-  moveElement(redBox, {
-    x: redTargetX,
-    y: redTargetY,
-  })
-    .then(() => console.log("Red Box moved"))
-    .then(() =>
-      moveElement(blueBox, {
-        x: blueTargetX,
-        y: blueTargetY,
-      })
-    )
-    .then(() => console.log("Blue Box moved"))
-    .then(() =>
-      moveElement(greenBox, {
-        x: greenTargetX,
-        y: greenTargetY,
-      })
-    )
-    .then(() => console.log("Green Box moved"));
-}
-
-translateOneByOne();
-
-//Comment out whichever you'd like to use
-
-// function translateAllAtOnce() {
-//   try {
-//     const redBoxPromise = moveElement(redBox, {
-//       x: redTargetX,
-//       y: redTargetY,
-//     });
-//     const blueBoxPromise = moveElement(blueBox, {
-//       x: blueTargetX,
-//       y: blueTargetY,
-//     });
-//     const greenBoxPromise = moveElement(greenBox, {
-//       x: greenTargetX,
-//       y: greenTargetY,
-//     });
-//     Promise.all([redBoxPromise, blueBoxPromise, greenBoxPromise]).then(() => {
-//       console.log("All elements have been moved");
-//     });
-//   } catch (error) {
-//     console.log(error);
-//   }
+// function translateOneByOne() {
+//   moveElement(redBox, redTarget)
+//     .then(() => console.log("Red Box moved"))
+//     .then(() => moveElement(blueBox, blueTarget))
+//     .then(() => console.log("Blue Box moved"))
+//     .then(() => moveElement(greenBox, greenTarget))
+//     .then(() => console.log("Green Box moved"));
 // }
 
-// translateAllAtOnce();
+// translateOneByOne();
+
+// Comment out whichever you'd like to use
+
+function translateAllAtOnce() {
+  const movements = [
+    moveElement(redBox, redTarget),
+    moveElement(blueBox, blueTarget),
+    moveElement(greenBox, greenTarget),
+  ];
+  Promise.all(movements).then(() => {
+    console.log("All elements have been moved");
+  });
+}
+
+translateAllAtOnce();

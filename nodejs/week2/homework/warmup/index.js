a12a000bb25b0dd2c43513a432dcc6f8a2afbd12
rebeccahockley <hockley.r@yahoo.com>
2022-04-06 22:48:57 +0200

    Warm Up Task Re-Upload

--- nodejs/week2/homework/warmup/index.js
+++ nodejs/week2/homework/warmup/index.js
@@ -4,24 +4,26 @@ const app = express();
 app.get("/", (req, res) => res.send("nodejs week2 homework"));
 
 app.get("/numbers/add", async (request, response) => {
-  const first = parseInt(request.query.first);
-  const second = parseInt(request.query.second);
-  const answer = first + second;
-  if (isNaN(first) || isNaN(second)) {
-    response.send(`Error! Not a number. Please enter 2 numbers`);
+  if (isNaN(request.query.first) || isNaN(request.query.second)) {
+    response.status(406).json(`Error! Not a number. Please enter 2 numbers`);
   } else {
-    response.send(`Answer: ${answer}`);
+    response.send(
+      `Answer: ${
+        parseInt(request.query.first) + parseInt(request.query.second)
+      } `
+    );
   }
 });
 
 app.get("/numbers/multiply", async (request, response) => {
-  const first = parseInt(request.query.first);
-  const second = parseInt(request.query.second);
-  const answer = first * second;
-  if (isNaN(first) || isNaN(second)) {
-    response.send(`Error! Not a number. Please enter 2 numbers`);
+  if (isNaN(request.query.first) || isNaN(request.query.second)) {
+    response.status(406).json(`Error! Not a number. Please enter 2 numbers`);
   } else {
-    response.send(`Answer: ${answer}`);
+    response.send(
+      `Answer: ${
+        parseInt(request.query.first) * parseInt(request.query.second)
+      } `
+    );
   }
 });
 
